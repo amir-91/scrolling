2583b2c21e38dfa954fd66555e99b50346f8dd67
amir-91 <61394176+amir-91@users.noreply.github.com>
2021-11-06 01:25:57 +0200

    code editing

--- src/components/ArticleList.vue
+++ src/components/ArticleList.vue
@@ -1,11 +1,12 @@
 <template>
 <div>
-    <div class="container" v-for="article in articles" :key="article.id">
-          <h3>Name: {{article.name}}</h3>
-          <h5>email: {{article.email}}</h5>
-          <p>{{article.body}}</p>
+    <div class="container" v-for="post in posts" :key="post.id">
+          <h3>Name: {{post.name}}</h3>
+          <h5>email: {{post.email}}</h5>
+          <p>{{post.body}}</p>
     </div>
        <div ref="infinitescrolltrigger" id="scoll-trigger"></div>
+       <div class="loader" v-if="loading"></div>
     </div>
 </template>
 
@@ -17,37 +18,48 @@ import axios from 'axios'
 
 export default {
     data () {
-     return  {
-         articles: [],
-         observer: null,
-     }
-    },
-    computed: {
-      pageCount() {
-        return Math.ceil(this.totalResults/this.maxPerPage);
-      },
-      pageOffset() {
-        return this.maxPerPage * this.currentPage;
+      return {
+           posts: [],
+           limit: 10,
+           loading: false,
+           lastDataId: 0,
+           lastPostId: 0,
       }
-    },
+  },
+
     methods: {
-      callback(entries) {
+    loadMore() {
+      if(this.lastElementId === this.lastPostId) {
+          return
+        }
+      this.loading = true;
+      axios.get("https://jsonplaceholder.typicode.com/comments").then(response => {
+        this.lastElementId = response.data[response.data.length-1].id
+        console.log(this.lastElementId)
+        const append = response.data.slice(
+          this.posts.length,
+          this.posts.length + this.limit
+        );
+        this.posts = this.posts.concat(append);
+        this.lastPostId = this.posts[this.posts.length-1].id
+        console.log(this.lastPostId)
+        this.loading = false;
+      });
+    },
+     callback(entries) {
       entries.forEach(entry => {
     if (entry.isIntersecting) {
-       this.fetchUsers();
+       this.loadMore();
     }
   });
 },
-       fetchUsers() {
-         for (var i = 0; i < 5; i++) {
-        axios.get(`https://jsonplaceholder.typicode.com/comments`).then(response => {
-          this.articles.push(response.data[i]);
-          this.showloader = false;
-        });
-      }
-      },
-    },
-    mounted() {
+},
+     created() {
+       this.loadMore();
+     },
+    
+
+  mounted() {
       this.observer = new IntersectionObserver(this.callback);
       this.observer.observe(this.$refs.infinitescrolltrigger);
 }
@@ -65,5 +77,19 @@ export default {
       border-radius: 10px;
       text-align: left;
       margin-bottom: 10px;
-   }
+}
+.loader {
+  border: 16px solid #f3f3f3; /* Light grey */
+  border-top: 16px solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin 2s linear infinite;
+  margin: auto;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
 </style>
